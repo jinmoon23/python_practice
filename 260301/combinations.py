d9fef60868fd00f4b22a5f905e4a686b2aa456de
jinmun23 <wlsansdlek1@me.com>
2026-03-01 15:51:41 +0900

    py_practice: day_2

---
+++ 260301/combinations.py
@@ -0,0 +1,17 @@
+def combinations(arr, r):
+  result = []
+  picked = []
+
+  def backtrack(start):
+    if (len(picked) == r):
+      result.append(picked[:])
+      return
+    
+    for i in range(start, len(arr)):
+      picked.append(arr[i])
+      backtrack(i + 1)
+      picked.pop()
+  
+  backtrack(0)
+  return result
+print(combinations(["a", "b", "c", "d", "e"], 2))
---
+++ 260301/tree_loop.py
@@ -0,0 +1,34 @@
+def preorder(nodes, idx, result):
+  if (idx >= len(nodes)):
+    return result
+  
+  result.append(nodes[idx])
+  preorder(nodes, idx * 2 + 1, result)
+  preorder(nodes, idx * 2 + 2, result)
+
+  return result
+
+def inorder(nodes, idx, result):
+  if (idx >= len(nodes)):
+    return result
+  
+  inorder(nodes, idx * 2 + 1, result)
+  result.append(nodes[idx])
+  inorder(nodes, idx * 2 + 2, result)
+
+  return result
+
+def postorder(nodes, idx, result):
+  if (idx >= len(nodes)):
+    return result
+  
+  postorder(nodes, idx * 2 + 1, result)
+  postorder(nodes, idx * 2 + 2, result)
+  result.append(nodes[idx])
+
+  return result
+
+
+print(preorder([1,2,3,4,5,6,7], 0, []))
+print(inorder([1,2,3,4,5,6,7], 0, []))
+print(postorder([1,2,3,4,5,6,7], 0, []))
